0bb1f87ff9ad18565a5fd9d1a2d86f2b7561f839
Hatice Özkaya <ozkyyhatice@gmail.com>
2026-03-13 12:48:46 +0300

    Add headless ChromeDriver support and CI workflow for Maven

--- src/main/java/com/example/driver/DriverFactory.java
+++ src/main/java/com/example/driver/DriverFactory.java
@@ -2,13 +2,21 @@ package com.example.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void initDriver() {
-        driver.set(new ChromeDriver());
+        String headless = System.getProperty("headless", "false"); // Maven'dan gelen parametreyi oku
+        ChromeOptions options = new ChromeOptions();
+        if (headless.equalsIgnoreCase("true")) {
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+        driver.set(new ChromeDriver(options));
         getDriver().manage().window().maximize();
     }
 
